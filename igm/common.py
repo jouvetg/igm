15dda317f1b08bcd75d20637b10ce5fc90b1b888
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2023-08-03 12:03:52 +0200

    upd

--- igm/common.py
+++ igm/common.py
@@ -5,7 +5,7 @@ Copyright (C) 2021-2023 Guillaume Jouvet <guillaume.jouvet@unil.ch>
 Published under the GNU GPL (Version 3), check at the LICENSE file
 """
 
-import os
+import os, glob
 
 # The (empty) class state serves in fact to use a dictionnary, but it looks like a class
 class State:
@@ -61,3 +61,30 @@ def print_params(params):
             print("%30s : %s" % (ck, params.__dict__[ck]), file=f)
 
     os.system("echo rm " + "igm-run-parameters.txt" + " >> clean.sh")
+
+# read a file and return a list of words
+def _read_file_to_list(file_path):
+    word_list = []
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            for line in file:
+                 # Remove leading/trailing whitespaces and newline characters
+                word = line.strip() 
+                word_list.append(word)
+    return word_list
+
+# load modules from files
+def load_modules():
+
+    L = glob.glob("modules_*.dat")
+
+    if len(L)>0:
+        modules_preproc  = _read_file_to_list('modules_preproc.dat')
+        modules_process  = _read_file_to_list('modules_process.dat')
+        modules_postproc = _read_file_to_list('modules_postproc.dat')
+    else:
+        modules_preproc  = ["prepare_data"]
+        modules_process  = ["mysmb","flow_dt_thk"]
+        modules_preproc  = ["write_ncdf_ex","write_plot2d","print_info","print_all_comp_info"]
+    
+    return modules_preproc, modules_process, modules_postproc

fd022a6346b31b138538c8dad85adabd2910ff73
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2024-04-28 11:40:52 +0200

    add test_vert_flow

---
+++ tests/test_vert_flow/test_vert_flow.py
@@ -0,0 +1,35 @@
+import igm
+import tensorflow as tf
+import pytest
+ 
+state = igm.State()
+modules_dict = {'modules_preproc': [], 'modules_process': ["iceflow","vert_flow"], 'modules_postproc': []}
+ 
+modules = igm.load_modules(modules_dict)
+
+parser = igm.params_core()
+
+for module in modules:
+    module.params(parser)
+
+params, __ = parser.parse_known_args()
+
+Nz,Ny,Nx = 10,40,30
+
+state.thk   = tf.Variable(tf.ones((Ny,Nx))*200)
+state.topg  = tf.Variable(tf.zeros((Ny,Nx)))
+state.usurf = state.thk + state.topg
+state.dX    = tf.Variable(tf.ones((Ny,Nx))*100) 
+state.dx    = 100
+state.it    = -1
+ 
+for module in modules:
+    module.initialize(params, state)
+
+for module in modules:
+    module.update(params, state)
+
+for module in modules:
+    module.finalize(params, state)
+    
+assert (tf.reduce_mean(state.W).numpy()<10*10)

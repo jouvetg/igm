b8c57b4d643eb1987251f27ad374538903533913
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2024-11-15 08:11:21 +0100

    add option to print memory info with print_mem_info in print_info

--- igm/modules/postproc/print_info/print_info.py
+++ igm/modules/postproc/print_info/print_info.py
@@ -7,16 +7,27 @@ import numpy as np
 import os
 import datetime
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
+from igm.modules.utils import *
 
 def params(parser):
-    pass
-
+    parser.add_argument(
+        "--print_mem_info",
+        type=str2bool,
+        default=False,
+        help="Additional info on memory usage",
+    )
 
 def initialize(params, state):
-    print(
-        "IGM %s :         Iterations   |         Time (y)     |     Time Step (y)   |   Ice Volume (km^3) "
-    )
+    if params.print_mem_info:
+        print(
+            "IGM %s :         Iterations   |         Time (y)     |     Time Step (y)   |   Ice Volume (km^3)   |   GPU memory (MB) "
+        )
+    else:
+        print(
+            "IGM %s :         Iterations   |         Time (y)     |     Time Step (y)   |   Ice Volume (km^3) "
+        )
 
 
 def update(params, state):
@@ -24,17 +35,31 @@ def update(params, state):
     This serves to print key info on the fly during computation
     """
     if state.saveresult:
-        print(
-            "IGM %s :      %6.0f    |      %8.0f        |     %7.2f        |     %10.2f "
-            % (
-                datetime.datetime.now().strftime("%H:%M:%S"),
-                state.it,
-                state.t,
-                state.dt_target,
-                np.sum(state.thk) * (state.dx**2) / 10**9,
-            )
-        )
 
+        if params.print_mem_info:
+            gpu_info = tf.config.experimental.get_memory_info("GPU:0")
+            print(
+                "IGM %s :      %6.0f    |      %8.0f        |     %7.2f        |     %10.2f         |     %10.2f "
+                % (
+                    datetime.datetime.now().strftime("%H:%M:%S"),
+                    state.it,
+                    state.t,
+                    state.dt_target,
+                    np.sum(state.thk) * (state.dx**2) / 10**9,
+                    gpu_info['current'] / 1024**2
+                )
+            )
+        else:    
+            print(
+                "IGM %s :      %6.0f    |      %8.0f        |     %7.2f        |     %10.2f "
+                % (
+                    datetime.datetime.now().strftime("%H:%M:%S"),
+                    state.it,
+                    state.t,
+                    state.dt_target,
+                    np.sum(state.thk) * (state.dx**2) / 10**9
+                )
+            )
 
 def finalize(params, state):
     pass

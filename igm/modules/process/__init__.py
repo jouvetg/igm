bda2daad79ee54da04e7539dde44935e8cbd1342
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2025-01-17 14:37:52 +0100

    remove form preprocess, process, postprocess folders

--- igm/modules/process/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-# Copyright (C) 2021-2023 Guillaume Jouvet <guillaume.jouvet@unil.ch>
-# Published under the GNU GPL (Version 3), check at the LICENSE file
-
-# from . import iceflow

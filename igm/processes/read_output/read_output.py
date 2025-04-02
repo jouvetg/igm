bda87b0c709cbd8dc0a47cd7d64f1f06b301ca7f
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2025-04-03 00:14:52 +0200

    use xarray instead of netcdf4 librairy

--- igm/processes/read_output/read_output.py
+++ igm/processes/read_output/read_output.py
@@ -4,58 +4,68 @@
 # Published under the GNU GPL (Version 3), check at the LICENSE file
 
 import numpy as np
-import os 
 import tensorflow as tf
-from netCDF4 import Dataset
- 
-from igm.processes.utils import *
- 
+import xarray as xr 
+import os
+
 def initialize(cfg, state):
- 
-    nc = Dataset(cfg.processes.read_output.input_file, "r")
 
-    state.x    = np.squeeze(nc.variables["x"]).astype("float32")
-    state.y    = np.squeeze(nc.variables["y"]).astype("float32")
-    state.time = np.squeeze(nc.variables["time"]).astype("float32")
+    # Lazy loading with chunking in time
+    ds = xr.open_dataset(os.path.join(state.original_cwd,cfg.processes.read_output.input_file), 
+                         chunks={"time": 1}) # comment this line if you don't want to install dask
 
-    state.list_var = [l for l in nc.variables if not l in ["x", "y", "z", "time"]]
+    # Read coordinates and time
+    state.x = ds["x"].values.astype("float32")
+    state.y = ds["y"].values.astype("float32")
+    state.time = ds["time"].values.astype("float32")
 
-    for var in state.list_var:
-        vars(state)[var+'_'+'t'] = np.squeeze(nc.variables[var]).astype("float32")
- 
-    # crop if requested
+    state.list_var = [v for v in ds.data_vars if v not in ["x", "y", "z", "time"]]
+
+    # Crop if needed
     if cfg.processes.read_output.crop:
+        x = state.x
+        y = state.y
         i0 = max(0, int((cfg.processes.read_output.xmin - x[0]) / (x[1] - x[0])))
         i1 = min(int((cfg.processes.read_output.xmax - x[0]) / (x[1] - x[0])), x.shape[0] - 1)
         i1 = max(i0 + 1, i1)
         j0 = max(0, int((cfg.processes.read_output.ymin - y[0]) / (y[1] - y[0])))
         j1 = min(int((cfg.processes.read_output.ymax - y[0]) / (y[1] - y[0])), y.shape[0] - 1)
-        j1 = max(j0 + 1, j1) 
-        for var in state.list_var:
-            vars(state)[var+'_'+'t'] = vars(state)[var+'_'+'t'][j0:j1, i0:i1]
-        y = y[j0:j1]
-        x = x[i0:i1]
-        
-    for var in ["x", "y"]:
-        vars(state)[var] = tf.constant(vars(state)[var].astype("float32"))
+        j1 = max(j0 + 1, j1)
+        state.crop = dict(x=slice(i0, i1), y=slice(j0, j1))
+        state.x = state.x[i0:i1]
+        state.y = state.y[j0:j1]
+    else:
+        state.crop = None
 
-    nc.close()
-    
-    cfg.processes.time.start = state.time[0]
-    cfg.processes.time.end   = state.time[-1]
+    # Store lazy-loaded dataset for later
+    state.ds = ds
+
+    # Save x and y as constants
+    state.x = tf.constant(state.x.astype("float32"))
+    state.y = tf.constant(state.y.astype("float32"))
+
+    start = state.time[0]
+    end = state.time[-1]
      
-    state.t = tf.Variable(float(cfg.processes.time.start))
+    state.t = tf.Variable(float(start))
     state.it = 0
     state.saveresult = True
-
+    state.dt = state.time[1] - state.time[0]
+    state.dx = state.x[1] - state.x[0]
+ 
 def update(cfg, state):
-    
     state.t.assign(state.time[state.it])
-     
-    for var in state.list_var:
-        vars(state)[var] = tf.Variable(vars(state)[var+'_'+'t'][state.it]) 
 
-    state.it += 1
+    for var in state.list_var:
+        if state.crop:
+            data = state.ds[var].isel(time=state.it, x=state.crop["x"], y=state.crop["y"])
+        else:
+            data = state.ds[var].isel(time=state.it)
+        vars(state)[var] = tf.Variable(data.values.astype("float32"))
+
+    if (state.it >= state.time.shape[0] - 1):
+        state.continue_run = False
 
 def finalize(cfg, state):
-    pass
+    # Clean up if needed (not strictly necessary with xarray)
+    state.ds.close()
